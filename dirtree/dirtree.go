5a26cf0e7de4adb3b8322f2e978a2eee18c2f7fe
John Turner <johnturner@me.com>
2022-08-28 17:10:26 +0000

    refactor: extract tree into structure
    This'll make it easier to add description info later

--- dirtree/dirtree.go
+++ dirtree/dirtree.go
@@ -1,11 +1,15 @@
 // package dirtree implements a tree structure of directories
 package dirtree
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+	"sort"
+)
 
 type Node struct {
 	Name     string
-	Children []Node
+	Children []*Node
 }
 
 func New(path string) (*Node, error) {
@@ -16,5 +20,39 @@ func New(path string) (*Node, error) {
 	if !fi.IsDir() {
 		return nil, nil
 	}
-	return &Node{Name: fi.Name()}, nil
+
+	n := Node{Name: fi.Name(), Children: []*Node{}}
+
+	dir, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	names, err := dir.Readdirnames(-1)
+	if err != nil {
+		return nil, err
+	}
+	_ = dir.Close()
+
+	names = filterNonDirs(path, names)
+	sort.Strings(names)
+	for _, name := range names {
+		child, err := New(filepath.Join(path, name))
+		if err != nil {
+			return nil, err
+		}
+		n.Children = append(n.Children, child)
+	}
+
+	return &n, nil
+}
+
+func filterNonDirs(path string, candidates []string) []string {
+	var dirs []string
+	for _, candidate := range candidates {
+		fi, _ := os.Stat(filepath.Join(path, candidate))
+		if fi.IsDir() {
+			dirs = append(dirs, candidate)
+		}
+	}
+	return dirs
 }
--- dirtree/dirtree_test.go
+++ dirtree/dirtree_test.go
@@ -17,3 +17,49 @@ func TestRead(t *testing.T) {
 		t.Fatalf("expected name 'a', got %v", got.Name)
 	}
 }
+
+func TestChildren(t *testing.T) {
+	root, err := dirtree.New("testdata/a")
+
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	children := root.Children
+	if len(children) != 2 {
+		t.Fatalf("expected node A to have 2 children, had %d", len(children))
+	}
+
+	if children[0].Name != "aa" {
+		t.Fatalf("expected first child of node A to have name aa, had %s", children[0].Name)
+	}
+
+	if children[1].Name != "bb" {
+		t.Fatalf("expected second child of node A to have name bb, had %s", children[1].Name)
+	}
+}
+
+func TestGrandChildren(t *testing.T) {
+	root, err := dirtree.New("testdata/a")
+
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	grandchildren := root.Children[0].Children
+	if len(grandchildren) != 3 {
+		t.Fatalf("expected node A to have 3 grandchildren from node AA, had %d", len(grandchildren))
+	}
+
+	if grandchildren[0].Name != "aaa" {
+		t.Fatalf("expected first child of node AA to have name aaa, had %s", grandchildren[0].Name)
+	}
+
+	if grandchildren[1].Name != "bbb" {
+		t.Fatalf("expected second child of node AA to have name bbb, had %s", grandchildren[1].Name)
+	}
+
+	if grandchildren[2].Name != "ccc" {
+		t.Fatalf("expected third child of node AA to have name ccc, had %s", grandchildren[2].Name)
+	}
+}
