5aeea05fb0efb2f845686d4dd5119f3e9933327e
John Turner <johnturner@me.com>
2022-08-29 03:42:59 +0000

    refactor: pull "visit" into dirtree package
    This commit pulls the visit function to be an implementation detail of printing a tree as a string.

--- cmd/main.go
+++ cmd/main.go
@@ -22,24 +22,6 @@ func run() error {
 	if err != nil {
 		return err
 	}
-	return visit(root, "")
-}
-
-// based on https://github.com/campoy/tools/tree
-func visit(node *dirtree.Node, indent string) error {
-	fmt.Println(node.Name)
-	add := "│   "
-	for i, child := range node.Children {
-		if i == len(node.Children)-1 {
-			fmt.Printf(indent + "└── ")
-			add = "    "
-		} else {
-			fmt.Printf(indent + "├── ")
-		}
-		err := visit(child, indent+add)
-		if err != nil {
-			return err
-		}
-	}
+	fmt.Print(root.String())
 	return nil
 }
--- dirtree/dirtree.go
+++ dirtree/dirtree.go
@@ -2,9 +2,12 @@
 package dirtree
 
 import (
+	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"sort"
+	"strings"
 )
 
 type Node struct {
@@ -44,6 +47,31 @@ func New(path string) (*Node, error) {
 	}
 
 	return &n, nil
+}
+
+func (n *Node) String() string {
+	var s strings.Builder
+	visit(&s, n, "")
+	return s.String()
+}
+
+// based on https://github.com/campoy/tools/tree
+func visit(w io.Writer, node *Node, indent string) error {
+	fmt.Fprintln(w, node.Name)
+	add := "│   "
+	for i, child := range node.Children {
+		if i == len(node.Children)-1 {
+			fmt.Fprintf(w, indent+"└── ")
+			add = "    "
+		} else {
+			fmt.Fprintf(w, indent+"├── ")
+		}
+		err := visit(w, child, indent+add)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func filterNonDirs(path string, candidates []string) []string {
